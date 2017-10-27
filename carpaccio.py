479d00b3406983d191b7b9e5dabd4f6a653136e7
Nils Wisiol <info@nils-wisiol.de>
2017-10-27 14:56:57 +0200

    2

--- carpaccio.py
+++ carpaccio.py
@@ -1,3 +1,11 @@
 import sys
 
-input("item price? ")
+price = 0
+try:
+    price = float(input("item price? "))
+except ValueError:
+    print("could not understand price")
+    exit(1)
+
+if price < 0:
+    print("price must be non-negative")
