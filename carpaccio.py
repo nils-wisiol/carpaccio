c5d327ff34fbf9392a70705f673d2edf7044a793
Nils Wisiol <info@nils-wisiol.de>
2017-10-27 15:11:40 +0200

    7

--- carpaccio.py
+++ carpaccio.py
@@ -1,25 +1,32 @@
 import sys
 
-def user_input(name):
+
+def user_input(name, input_type):
     value = 0
     try:
-        value = float(input("%s? " % name))
+        value = input_type(input("%s? " % name))
     except ValueError:
         print("could not understand %s" % name)
-        exit(1)
-
-    if value < 0:
-        print("%s must be non-negative" % name)
         exit(1)
 
     return value
 
 
-price = user_input("price")
+def check_nonnegative(value, name):
+    if value < 0:
+        print("%s must be non-negative" % name)
+        exit(1)
+    return value
+
+
+price = check_nonnegative(user_input("price", float), "price")
 print("item price: %.2f" % price)
 
-qty = user_input("quantity")
+qty = check_nonnegative(user_input("quantity", float), "quantity")
 print("quantity: %.2f" % qty)
+
+state = user_input("state", str)
+print("state: %s" % state)
 
 total = price * qty
 print("\ntotal price: %.2f" % total)
